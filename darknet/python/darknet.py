da090beddb55d6fed7724b8d561127fb2a209ac5
Chennamolla Vinay <56769454+nicky7767@users.noreply.github.com>
2022-06-09 10:00:05 +0530

    Update darknet.py

--- darknet/python/darknet.py
+++ darknet/python/darknet.py
@@ -46,7 +46,7 @@ class METADATA(Structure):
     
 
 #lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("darknet/libdarknet.so", RTLD_GLOBAL)
+lib = CDLL("/home/nicky/darknet/libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
@@ -167,15 +167,15 @@ def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
         free_detections(dets, num)
     return res,wh
     
-if __name__ == "__main__":
-    #net = load_net("cfg/densenet201.cfg", "/home/pjreddie/trained/densenet201.weights", 0)
-    #im = load_image("data/wolf.jpg", 0, 0)
-    #meta = load_meta("cfg/imagenet1k.data")
-    #r = classify(net, meta, im)
-    #print r[:10]
-    net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
-    meta = load_meta("cfg/coco.data")
-    r = detect(net, meta, "data/dog.jpg")
-    print r
+# if __name__ == "__main__":
+#     #net = load_net("cfg/densenet201.cfg", "/home/pjreddie/trained/densenet201.weights", 0)
+#     #im = load_image("data/wolf.jpg", 0, 0)
+#     #meta = load_meta("cfg/imagenet1k.data")
+#     #r = classify(net, meta, im)
+#     #print r[:10]
+#     net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
+#     meta = load_meta("cfg/coco.data")
+#     r = detect(net, meta, "data/dog.jpg")
+#     print(r)
     
 
